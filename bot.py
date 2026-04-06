7d0ad148d9a177bfa39b6b2f50dd3a631e650b98
serro55 <serateacher5@hotmail.com>
2026-04-06 22:49:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -96,6 +96,16 @@ inşaAllah kanal linkini aileniz ve arkadaşlarınızla paylaşabilirsiniz.
 BarakAllah fikom.""",
         reply_markup=InlineKeyboardMarkup(student_keyboard)
     )
+    await context.bot.send_message(
+    chat_id=user.id,
+    text="""📩 Mesajınız bize ulaştı
+
+🌷 MaşaAllah, gayretiniz çok kıymetli.
+Allah izniyle en kısa sürede değerlendirilecek ve size dönüş yapılacaktır.
+
+İnşaAllah bu yolculukta adım adım ilerleyeceksiniz.
+Allah muvaffak eylesin 🤍"""
+    )
 
 # ⭐ عرض المستويات
 async def handle_rate(update: Update, context: ContextTypes.DEFAULT_TYPE):
